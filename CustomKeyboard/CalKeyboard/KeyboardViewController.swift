13fdf96d24dcd252b241214818fceae2a207945b
Winnie Wen <wiwen@berkeley.edu>
2015-09-23 23:03:23 -0700

    Submitting hw2

--- CustomKeyboard/CalKeyboard/KeyboardViewController.swift
+++ CustomKeyboard/CalKeyboard/KeyboardViewController.swift
@@ -8,9 +8,14 @@
 
 import UIKit
 
-class KeyboardViewController: UIInputViewController {
+class KeyboardViewController: UIInputViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var smsKeyboardButton: UIButton!
+    @IBOutlet weak var twitterKeyboardButton: UIButton!
     @IBOutlet var nextKeyboardButton: UIButton!
+    @IBOutlet weak var returnKeyboardButton: UIButton!
+    @IBOutlet weak var deleteKeyboardButton: UIButton!
+    @IBOutlet weak var emailKeyboardButton: UIButton!
     
     var keyboardView: UIView!
 
@@ -37,6 +42,32 @@ class KeyboardViewController: UIInputViewController {
     override func textDidChange(textInput: UITextInput?) {
         // The app has just changed the document's contents, the document context has been updated.
     }
+    
+    func inputForTwitter() {
+        let text = self.textDocumentProxy as UITextDocumentProxy
+        text.insertText("@username")
+    }
+    func inputForReturn() {
+        let text = self.textDocumentProxy as UITextDocumentProxy
+        text.insertText("\n ")
+    }
+    
+    func inputForDelete() {
+        let text = self.textDocumentProxy as UITextDocumentProxy
+        text.deleteBackward()
+    }
+    
+    func inputForEmail() {
+        let text = self.textDocumentProxy as UITextDocumentProxy
+        text.insertText("winniewenw@gmail.com")
+    }
+    
+    func inputForTextnumber() {
+        let text = self.textDocumentProxy as UITextDocumentProxy
+        text.insertText("415-XXX-XXXX")
+    }
+    
+    
 
     func loadInterface() {
         let keyboardNib = UINib(nibName: "Keyboard", bundle: nil)
@@ -45,6 +76,12 @@ class KeyboardViewController: UIInputViewController {
         view.addSubview(keyboardView)
         view.backgroundColor = keyboardView.backgroundColor
         nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside) // advanceToNextInputMode is already defined in template
+        returnKeyboardButton.addTarget(self, action: "inputForReturn", forControlEvents: .TouchUpInside)
+        deleteKeyboardButton.addTarget(self, action: "inputForDelete", forControlEvents: .TouchUpInside)
+        twitterKeyboardButton.addTarget(self, action: "inputForTwitter", forControlEvents: .TouchUpInside)
+        emailKeyboardButton.addTarget(self, action: "inputForEmail", forControlEvents: .TouchUpInside)
+        smsKeyboardButton.addTarget(self, action: "inputForTextnumber", forControlEvents: .TouchUpInside)
+        
     }
 
 
